eca6d70712964f30f76babe245e64ae87a88d3d4
rochebox <rochebox@gmail.com>
2016-08-20 17:09:26 +0000

    submit form button getting it to work

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -6,4 +6,15 @@ class BandsController < ApplicationController
 	def new
 		@band = Band.new
 	end
+
+	def create
+		Band.create(band_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def band_params
+	    params.require(:band).permit( :name, :members, :song1, :song2, :song3 )
+	end
 end
